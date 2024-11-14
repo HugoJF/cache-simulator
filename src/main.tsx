3c4768264d302f9c432978f86f2419a83cc2433d
Hugo Jeller Ferreira <hugo_jeller@hotmail.com>
2024-11-13 21:38:37 -0400

    feat: setup Sentry error monitoring

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
+import * as Sentry from "@sentry/react";
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import {ConfigProvider} from "antd";
 import {CachesProvider} from "./contexts/caches.tsx";
 import {ProgramsProvider} from "./contexts/programs.tsx";
 import {SettingsProvider} from "./contexts/settings.tsx";
+
+Sentry.init({
+    dsn: import.meta.env.VITE_SENTRY_DSN,
+    integrations: [],
+});
 
 // TODO move ConfigProvider to App.tsx
 ReactDOM.createRoot(document.getElementById('root')!).render(
