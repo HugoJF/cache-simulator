f9fc43d807b7c3a84b71aeaf5b21833248c59ea0
Hugo Jeller Ferreira <hugo_jeller@hotmail.com>
2024-11-22 09:12:14 -0400

    chore: avoid wiping font-mono from Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    fontFamily: {
-      sans: ['"Geist Sans"', 'sans-serif'],
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['"Geist Sans"', 'sans-serif'],
+            },
+        },
     },
-    extend: {},
-  },
-  plugins: [],
+    plugins: [],
 }
 
